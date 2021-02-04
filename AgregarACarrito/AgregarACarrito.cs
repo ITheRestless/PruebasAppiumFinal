361e6ec51744bba29321046ac3aee8ae4d6dff45
Mauricio Emmanuel Avitia Maturino <45672490+ITheRestless@users.noreply.github.com>
2021-02-04 17:59:05 -0600

    Terminado y funcionando Carrito y añadidos logs de mis direcciones

--- AgregarACarrito/AgregarACarrito.cs
+++ AgregarACarrito/AgregarACarrito.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using OpenQA.Selenium.Appium.Interfaces;
 using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium;
 
 namespace AgregarACarrito
 {
@@ -56,6 +57,19 @@ namespace AgregarACarrito
             touchAction.Perform();
         }
 
+        public void ClickText(string txt, AndroidDriver<AndroidElement> driver)
+        {
+            AndroidElement searchElement = (AndroidElement)new WebDriverWait(
+                driver, TimeSpan.FromSeconds(20)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(
+                    MobileBy.AndroidUIAutomator("new UiSelector().textContains(\"" + txt + "\")"))
+            );
+
+
+
+            searchElement.Click();
+        }
+
         public void InputText(string id, string text, AndroidDriver<AndroidElement> driver)
         {
             AndroidElement searchElement = (AndroidElement)new WebDriverWait(
@@ -75,6 +89,17 @@ namespace AgregarACarrito
                 driver, TimeSpan.FromSeconds(20)).Until(
                 SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(
                     MobileBy.Id(id))
+            );
+
+            searchElement.Click();
+        }
+
+        public void ClickClass(string clss, AndroidDriver<AndroidElement> driver)
+        {
+            AndroidElement searchElement = (AndroidElement)new WebDriverWait(
+                driver, TimeSpan.FromSeconds(20)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(
+                    MobileBy.ClassName(clss))
             );
 
             searchElement.Click();
@@ -100,6 +125,29 @@ namespace AgregarACarrito
             }
         }
 
+        public bool CheckText(string txt, AndroidDriver<AndroidElement> driver)
+        {
+            AndroidElement searchElement = (AndroidElement)new WebDriverWait(
+                driver, TimeSpan.FromSeconds(10)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(
+                    MobileBy.AndroidUIAutomator("new UiSelector().textContains(\"" + txt + "\")"))
+            );
+
+            if (searchElement == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void setState(string state, string arguments, AndroidDriver<AndroidElement> driver)
+        {
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"" + state + "\", \"reason\": \" " + arguments + " \"}}");
+        }
+
         public void StartTimer()
         {
             timer = Stopwatch.StartNew();
@@ -119,7 +167,53 @@ namespace AgregarACarrito
         [TestMethod]
         public void DetalleDeArticulo()
         {
+            CapsInit();
+            caps.AddAdditionalCapability("name", "Carrito - Detalle de Articulo");
 
+            AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(
+                    new Uri("http://hub-cloud.browserstack.com/wd/hub"), caps);
+
+
+            //--------------------------Secuencia----------------------------------
+            StartTimer();
+
+            ClickButton("com.soriana.appsoriana:id/imgArrow", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o no se pudo presionar el boton de inicio \"}}");
+            ClickButton("com.soriana.appsoriana:id/btnIniciaSesion", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o no se pudo llenar el campo de email \"}}");
+            InputText("com.soriana.appsoriana:id/editEmail", "autodevelopmx@gmail.com", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o no se pudo llenar el campo de contraseña \"}}");
+            InputText("com.soriana.appsoriana:id/editPass", "developmx12", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o no se pudo presionar boton de LogIn \"}}");
+            ClickButton("com.soriana.appsoriana:id/btn_login", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o pudo llenar el campo para buscar un producto \"}}");
+            InputText("android:id/search_src_text", "agua natural ciel", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o encontro el producto --AGUA NATURAL CIEL 1LT-- \"}}");
+            ClickText("AGUA NATURAL CIEL 1 LT", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se pudo hacer scroll de pantalla \"}}");
+            ScrollDown(driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o pudo presionar el boton --Agregar al carrito-- \"}}");
+            ClickButton("com.soriana.appsoriana:id/txtButtonAddCarrito", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se  mostro o pudo presionar el boton --Domicilio-- \"}}");
+            ClickButton("com.soriana.appsoriana:id/txtDomicilio", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se encontro o pudo llenar el campo --Codigo Postal-- \"}}");
+            InputText("com.soriana.appsoriana:id/etCodigoPostal", "27268", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se  mostro o pudo presionar el boton --Seleccionar-- \"}}");
+            ClickButton("com.soriana.appsoriana:id/btnSeleccionar", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se  mostro o pudo presionar el icono de carrito \"}}");
+            driver.HideKeyboard();
+            driver.HideKeyboard();
+            driver.HideKeyboard();
+            ClickButton("com.soriana.appsoriana:id/imageCart", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se agrego el articulo al carrito \"}}");
+            ClickText("AGUA NATURAL CIEL 1LT", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o pudó presionar el boton --volver-- \"}}");
+            ClickClass("android.widget.ImageButton", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se pudo borrar el articulo agregado \"}}");
+            ClickButton("com.soriana.appsoriana:id/action_delete", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"passed\", \"reason\": \" El articulo se añadio y removio del carrito con exito \"}}");
+
+            driver.Quit();
         }
 
         [TestMethod]
@@ -131,7 +225,50 @@ namespace AgregarACarrito
         [TestMethod]
         public void MisListas()
         {
+            CapsInit();
+            caps.AddAdditionalCapability("name", "Carrito - Mis Listas");
 
+            AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(
+                    new Uri("http://hub-cloud.browserstack.com/wd/hub"), caps);
+
+
+            //--------------------------Secuencia----------------------------------
+            StartTimer();
+            ClickButton("com.soriana.appsoriana:id/imgArrow", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o no se pudo presionar el boton de inicio \"}}");
+            ClickButton("com.soriana.appsoriana:id/btnIniciaSesion", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o no se pudo llenar el campo de email \"}}");
+            InputText("com.soriana.appsoriana:id/editEmail", "autodevelopmx@gmail.com", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o no se pudo llenar el campo de contraseña \"}}");
+            InputText("com.soriana.appsoriana:id/editPass", "developmx12", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o no se pudo presionar boton de LogIn \"}}");
+            ClickButton("com.soriana.appsoriana:id/btn_login", driver);
+            setState("failed", "No se pudo acceder o encontrar la pestaña --Listas--", driver);
+            ClickButton("com.soriana.appsoriana:id/misListasFragment", driver);
+            setState("failed", "No se encontro o pudo acceder a la lista --Mis Favoritos--", driver);
+            ClickText("Mis Favoritos", driver);
+            setState("failed", "No se pudo marcar los articulos de la lista", driver);
+            ClickButton("com.soriana.appsoriana:id/select_all", driver);
+            setState("failed", "No se pudó presionar el boton --Añadir al carrito--", driver);
+            ClickButton("com.soriana.appsoriana:id/btnAddCart", driver);
+            setState("failed", "No se pudó aceptar la confirmacion de agregar al carrito", driver);
+            ClickButton("android:id/button1", driver);
+            setState("failed", "No se encontro el boton de volver", driver);
+            ClickButton("com.soriana.appsoriana:id/activity_main_content_button_back", driver);
+            setState("failed", "No se encontro el boton de home", driver);
+            ClickButton("com.soriana.appsoriana:id/nuevoInicioFragment", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se  mostro o pudo presionar el icono de carrito \"}}");
+            ClickButton("com.soriana.appsoriana:id/imageCart", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se agrego el articulo al carrito \"}}");
+            ClickText("AGUA NATURAL CIEL 1 LT", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o pudo presionar el boton --volver-- \"}}");
+            ClickClass("android.widget.ImageButton", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se pudo borrar el articulo agregado \"}}");
+            ClickButton("com.soriana.appsoriana:id/action_delete", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"passed\", \"reason\": \" El articulo se añadio y removio del carrito con exito \"}}");
+
+
+            driver.Quit();
         }
     }
 }
