d0e98cf4a0c4ecf2ce1d46e1fef5ea95dc0448d3
Mauricio Emmanuel Avitia Maturino <45672490+ITheRestless@users.noreply.github.com>
2021-02-11 18:00:10 -0600

    Añadido caso "SuperEnCasa" y corregido carrito para cambio de sucursal

---
+++ SuperEnCasa/SuperEnCasa.cs
@@ -0,0 +1,195 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+using System.Diagnostics;
+using OpenQA.Selenium.Appium.Interfaces;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium;
+
+namespace SuperEnCasa
+{
+    [TestClass]
+    public class SuperEnCasa
+    {
+        Stopwatch timer;
+        double time;
+        AppiumOptions caps;
+
+        public void CapsInit()
+        {
+            string fecha = DateTime.Now.Day.ToString() + "/" + DateTime.Now.Month.ToString() + "/" + DateTime.Now.Year.ToString();
+            caps = new AppiumOptions();
+            caps.AddAdditionalCapability("newCommandTimeout", 30);
+            caps.AddAdditionalCapability("browserstack.user", "mauricioemmanuel1");
+            caps.AddAdditionalCapability("browserstack.key", "XZYh6tFKBx8KBDyBzbAy");
+            caps.AddAdditionalCapability("autoAcceptAlerts", true);
+            caps.AddAdditionalCapability("autoGrantPermissions", true);
+            caps.AddAdditionalCapability("app", "bs://c23a88d88edd54c9d25811a2eed508f30f9ae1e7");
+            caps.AddAdditionalCapability("device", "Google Pixel 3");
+            caps.AddAdditionalCapability("os_version", "9.0");
+            caps.PlatformName = "Android";
+            caps.AddAdditionalCapability("project", "AppSoriana");
+            caps.AddAdditionalCapability("build", "Android " + fecha);
+        }
+
+        public void ScrollDown(AndroidDriver<AndroidElement> driver)
+        {
+            ITouchAction touchAction = new TouchAction(driver)
+            .Press(200, 1000)
+            .Wait(500)
+            .MoveTo(200, 200)
+            .Release();
+
+            touchAction.Perform();
+        }
+
+        public void ScrollUp(AndroidDriver<AndroidElement> driver)
+        {
+            ITouchAction touchAction = new TouchAction(driver)
+            .Press(200, 200)
+            .Wait(500)
+            .MoveTo(200, 1000)
+            .Release();
+
+            touchAction.Perform();
+        }
+
+        public void ClickText(string txt, AndroidDriver<AndroidElement> driver)
+        {
+            AndroidElement searchElement = (AndroidElement)new WebDriverWait(
+                driver, TimeSpan.FromSeconds(20)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(
+                    MobileBy.AndroidUIAutomator("new UiSelector().textContains(\"" + txt + "\")"))
+            );
+
+
+
+            searchElement.Click();
+        }
+
+        public void InputText(string id, string text, AndroidDriver<AndroidElement> driver)
+        {
+            AndroidElement searchElement = (AndroidElement)new WebDriverWait(
+                driver, TimeSpan.FromSeconds(20)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(
+                    MobileBy.Id(id))
+            );
+
+            searchElement.Click();
+            searchElement.Clear();
+            searchElement.SendKeys(text);
+            driver.HideKeyboard();
+        }
+
+        public void ClickButton(string id, AndroidDriver<AndroidElement> driver)
+        {
+            AndroidElement searchElement = (AndroidElement)new WebDriverWait(
+                driver, TimeSpan.FromSeconds(20)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(
+                    MobileBy.Id(id))
+            );
+
+            searchElement.Click();
+        }
+
+        public void ClickClass(string clss, AndroidDriver<AndroidElement> driver)
+        {
+            AndroidElement searchElement = (AndroidElement)new WebDriverWait(
+                driver, TimeSpan.FromSeconds(20)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(
+                    MobileBy.ClassName(clss))
+            );
+
+            searchElement.Click();
+        }
+
+        public bool CheckElement(string id, AndroidDriver<AndroidElement> driver)
+        {
+            AndroidElement searchElement = (AndroidElement)new WebDriverWait(
+                driver, TimeSpan.FromSeconds(10)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(
+                    MobileBy.Id(id))
+            );
+
+            if (searchElement == null)
+            {
+                Console.WriteLine("Salida inesperada");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Salida correcta");
+                return true;
+            }
+        }
+
+        public bool CheckText(string txt, AndroidDriver<AndroidElement> driver)
+        {
+            AndroidElement searchElement = (AndroidElement)new WebDriverWait(
+                driver, TimeSpan.FromSeconds(10)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(
+                    MobileBy.AndroidUIAutomator("new UiSelector().textContains(\"" + txt + "\")"))
+            );
+
+            if (searchElement == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void setState(string state, string arguments, AndroidDriver<AndroidElement> driver)
+        {
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"" + state + "\", \"reason\": \" " + arguments + " \"}}");
+        }
+
+        public void LogIn(AndroidDriver<AndroidElement> driver)
+        {
+            ClickButton("com.soriana.appsoriana:id/imgArrow", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o no se pudo presionar el boton de inicio \"}}");
+            ClickButton("com.soriana.appsoriana:id/btnIniciaSesion", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o no se pudo llenar el campo de email \"}}");
+            InputText("com.soriana.appsoriana:id/editEmail", "autodevelopmx@gmail.com", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o no se pudo llenar el campo de contraseña \"}}");
+            InputText("com.soriana.appsoriana:id/editPass", "developmx12", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o no se pudo presionar boton de LogIn \"}}");
+            ClickButton("com.soriana.appsoriana:id/btn_login", driver);
+        }
+
+        public void StartTimer()
+        {
+            timer = Stopwatch.StartNew();
+        }
+
+        public double ExecTime()
+        {
+            return timer.Elapsed.Seconds;
+        }
+
+        [TestMethod]
+        public void VerMas()
+        {
+            CapsInit();
+            caps.AddAdditionalCapability("name", "Super en casa - Ver mas");
+
+            AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(
+                    new Uri("http://hub-cloud.browserstack.com/wd/hub"), caps);
+
+            LogIn(driver);
+
+            setState("failed", "No encontrado boton --Ver mas--", driver);
+            ClickText("Ver más", driver);
+
+            setState("passed", "Boton --Ver mas-- Funcionando", driver);
+
+            driver.Quit();
+        }
+    }
+}
