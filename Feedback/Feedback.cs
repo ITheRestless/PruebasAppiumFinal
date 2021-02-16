db21c6bbee841a19b8fec5fca4b283cd306941cb
IvanARQuiroz <70453109+IvanARQuiroz@users.noreply.github.com>
2021-02-15 18:01:32 -0600

    Feedback

---
+++ Feedback/Feedback.cs
@@ -0,0 +1,209 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Interfaces;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Feedback
+{
+    [TestClass]
+    public class Feedback
+    {
+        Stopwatch timer;
+        double time;
+        AppiumOptions caps;
+
+        public void CapsInit()
+        {
+            string fecha = DateTime.Now.Day.ToString() + "/" + DateTime.Now.Month.ToString() + "/" + DateTime.Now.Year.ToString();
+            caps = new AppiumOptions();
+            caps.AddAdditionalCapability("newCommandTimeout", 30);
+            caps.AddAdditionalCapability("browserstack.user", "mauricioemmanuel1");
+            caps.AddAdditionalCapability("browserstack.key", "XZYh6tFKBx8KBDyBzbAy");
+            caps.AddAdditionalCapability("autoAcceptAlerts", true);
+            caps.AddAdditionalCapability("autoGrantPermissions", true);
+            caps.AddAdditionalCapability("app", "bs://c23a88d88edd54c9d25811a2eed508f30f9ae1e7");
+            caps.AddAdditionalCapability("device", "Google Pixel 3");
+            caps.AddAdditionalCapability("os_version", "9.0");
+            caps.PlatformName = "Android";
+            caps.AddAdditionalCapability("project", "AppSoriana");
+            caps.AddAdditionalCapability("build", "Android " + fecha);
+        }
+
+        public void ScrollDown(AndroidDriver<AndroidElement> driver)
+        {
+            ITouchAction touchAction = new TouchAction(driver)
+            .Press(200, 1000)
+            .Wait(500)
+            .MoveTo(200, 200)
+            .Release();
+
+            touchAction.Perform();
+        }
+
+        public void ScrollUp(AndroidDriver<AndroidElement> driver)
+        {
+            ITouchAction touchAction = new TouchAction(driver)
+            .Press(200, 200)
+            .Wait(500)
+            .MoveTo(200, 1000)
+            .Release();
+
+            touchAction.Perform();
+        }
+
+        public void ClickText(string txt, AndroidDriver<AndroidElement> driver)
+        {
+            AndroidElement searchElement = (AndroidElement)new WebDriverWait(
+                driver, TimeSpan.FromSeconds(20)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(
+                    MobileBy.AndroidUIAutomator("new UiSelector().textContains(\"" + txt + "\")"))
+            );
+
+
+
+            searchElement.Click();
+        }
+
+        public void InputText(string id, string text, AndroidDriver<AndroidElement> driver)
+        {
+            AndroidElement searchElement = (AndroidElement)new WebDriverWait(
+                driver, TimeSpan.FromSeconds(20)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(
+                    MobileBy.Id(id))
+            );
+
+            searchElement.Click();
+            searchElement.Clear();
+            searchElement.SendKeys(text);
+            driver.HideKeyboard();
+        }
+
+        public void ClickButton(string id, AndroidDriver<AndroidElement> driver)
+        {
+            AndroidElement searchElement = (AndroidElement)new WebDriverWait(
+                driver, TimeSpan.FromSeconds(20)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(
+                    MobileBy.Id(id))
+            );
+
+            searchElement.Click();
+        }
+
+        public void ClickClass(string clss, AndroidDriver<AndroidElement> driver)
+        {
+            AndroidElement searchElement = (AndroidElement)new WebDriverWait(
+                driver, TimeSpan.FromSeconds(20)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(
+                    MobileBy.ClassName(clss))
+            );
+
+            searchElement.Click();
+        }
+
+        public bool CheckElement(string id, AndroidDriver<AndroidElement> driver)
+        {
+            AndroidElement searchElement = (AndroidElement)new WebDriverWait(
+                driver, TimeSpan.FromSeconds(10)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(
+                    MobileBy.Id(id))
+            );
+
+            if (searchElement == null)
+            {
+                Console.WriteLine("Salida inesperada");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Salida correcta");
+                return true;
+            }
+        }
+
+        public void CheckText(string txt, AndroidDriver<AndroidElement> driver)
+        {
+            AndroidElement searchElement = (AndroidElement)new WebDriverWait(
+                driver, TimeSpan.FromSeconds(10)).Until(
+                SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(
+                    MobileBy.AndroidUIAutomator("new UiSelector().textContains(\"" + txt + "\")"))
+            );
+        }
+
+        public void setState(string state, string arguments, AndroidDriver<AndroidElement> driver)
+        {
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"" + state + "\", \"reason\": \" " + arguments + " \"}}");
+        }
+
+        public void LogIn(AndroidDriver<AndroidElement> driver)
+        {
+            ClickButton("com.soriana.appsoriana:id/imgArrow", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o no se pudo presionar el boton de inicio \"}}");
+            ClickButton("com.soriana.appsoriana:id/btnIniciaSesion", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o no se pudo llenar el campo de email \"}}");
+            InputText("com.soriana.appsoriana:id/editEmail", "autodevelopmx@gmail.com", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o no se pudo llenar el campo de contraseña \"}}");
+            InputText("com.soriana.appsoriana:id/editPass", "developmx12", driver);
+            ((IJavaScriptExecutor)driver).ExecuteScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\":\"failed\", \"reason\": \" No se mostro o no se pudo presionar boton de LogIn \"}}");
+            ClickButton("com.soriana.appsoriana:id/btn_login", driver);
+        }
+
+        public void StartTimer()
+        {
+            timer = Stopwatch.StartNew();
+        }
+
+        public double ExecTime()
+        {
+            return timer.Elapsed.Seconds;
+        }
+
+        [TestMethod]
+        public void EnviarFeedback()
+        {
+            CapsInit();
+            caps.AddAdditionalCapability("name", "Feedback - Enviar Feedback");
+
+            AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(
+                    new Uri("http://hub-cloud.browserstack.com/wd/hub"), caps);
+
+            LogIn(driver);
+
+            //--------------------------Secuencia----------------------------------
+            StartTimer();
+
+            setState("failed", "Boton --Menu-- no encontrado", driver);
+            ClickButton("com.soriana.appsoriana:id/menuFragment", driver);
+
+            setState("failed", "Seccion --Danos tu opinion-- no encontrada", driver);
+            ClickButton("com.soriana.appsoriana:id/item_feedback", driver);
+
+            setState("failed", "No fue posible seleccionar un emoji para calificar la experiencia", driver);
+            ClickButton("com.soriana.appsoriana:id/moodNeutral", driver);
+
+            setState("failed", "No fue posible seleccionar el tipo de comentario", driver);
+            ClickButton("com.soriana.appsoriana:id/rbOtro", driver);
+
+            setState("failed", "No fue posible escribir un comentario", driver);
+            InputText("com.soriana.appsoriana:id/editMensaje", "Prueba feedback", driver);
+
+            setState("failed", "Boton --Enviar-- no encontrado", driver);
+            ScrollDown(driver);
+            ClickButton("com.soriana.appsoriana:id/btnEnviarFeedback", driver);
+
+            Thread.Sleep(2000);
+
+            setState("failed", "Fallo al enviar el comentario", driver);
+            CheckText("Folletos", driver);
+
+            setState("passed", "Se enviaron los comentarios correctamente", driver);
+
+            driver.Quit();
+        }
+    }
+}
