81087eb7ee2da789760e0024e73db53bf7408c65
Mauricio Emmanuel Avitia Maturino <restlessdisaster@gmail.com>
2021-05-11 02:25:42 -0500

    PorFavorFuncionaYaQuieroDormir

---
+++ Carrito, Listas y Entregas/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Carrito, Listas y Entregas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Carrito, Listas y Entregas")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7d8a2bd6-022f-4b4c-8f89-188391611b81")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
